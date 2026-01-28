4571cc38df3b76f88eccfffd28a03dd803b220e9
DESKTOP-2E4LAF7\NeNikita <kolobikus@gmail.com>
2026-01-28 23:30:15 +0200

    HellFire_BloodFurnce/Broggok Posion Bolt timer + SpellID for normal

--- DBM-Party-BC/Hellfire_BloodFurnace/Broggok.lua
+++ DBM-Party-BC/Hellfire_BloodFurnace/Broggok.lua
@@ -1,4 +1,4 @@
-local mod   = DBM:NewMod("Broggok", "DBM-Party-BC", 2, 256)
+local mod	= DBM:NewMod("Broggok", "DBM-Party-BC", 2, 256)
 
 mod:SetRevision("20260128000000")
 mod:SetCreatureID(17380)
@@ -6,7 +6,7 @@ mod:SetModelID(19372)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-    "SPELL_CAST_SUCCESS 30916 38459" -- Добавить таймера обычки
+    "SPELL_CAST_SUCCESS 30916, 30917, 38459" -- Проверить таймер Ядовитого удара в обычке(30917)
 )
 
 local timerPoisonCloud  = mod:NewCDTimer(20, 30916)
