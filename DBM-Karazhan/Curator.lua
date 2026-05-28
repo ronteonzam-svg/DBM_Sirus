9a284f0b87c61519963c02490aed85abd0d447cd
DESKTOP-2E4LAF7\NeNikita <kolobikus@gmail.com>
2026-05-29 02:47:48 +0300

    Curator Fix Timers. Coil Slavepens last boss timers. Twins info panel

--- DBM-Karazhan/Curator.lua
+++ DBM-Karazhan/Curator.lua
@@ -1,64 +1,20 @@
 local mod = DBM:NewMod("Curator", "DBM-Karazhan")
 local L   = mod:GetLocalizedStrings()
 
-mod:SetRevision("20210502220000") -- fxpw check 202206151120000
+mod:SetRevision("20260528000000")
 mod:SetCreatureID(99973, 99972, 99974, 15691)
---mod:SetCreatureID(15691)
---mod:RegisterCombat("yell", L.DBM_CURA_YELL_PULL)
 mod:RegisterCombat("combat", 99974, 15691)
 
 mod:RegisterEvents(
 	"SPELL_AURA_APPLIED 305309 305305",
 	"SPELL_AURA_REMOVED 305313 305309 305305",
 	"SPELL_CAST_START 305296 305312",
-	-- "SPELL_INTERRUPT",
+	"SPELL_CAST_SUCCESS 305298 30254",
 	"CHAT_MSG_MONSTER_YELL"
 )
 
--- local warnEvoSoon		= mod:NewPreWarnAnnounce(30254, 10, 2)
--- local warnEvo			= mod:NewSpellAnnounce(30254, 3)
--- local warnArcaneInfusion= mod:NewSpellAnnounce(30403, 3)
---
--- local timerEvo			= mod:NewBuffActiveTimer(20, 30254)
--- local timerNextEvo		= mod:NewNextTimer(115, 30254)
---
--- local berserkTimer		= mod:NewBerserkTimer(720)
---
--- mod:AddBoolOption("RangeFrame", true)
---
--- function mod:OnCombatStart(delay)
--- 	berserkTimer:Start(-delay)
--- 	timerNextEvo:Start(109-delay)
--- 	warnEvoSoon:Schedule(99-delay)
--- 	if self.Options.RangeFrame then
--- 		DBM.RangeCheck:Show(10)
--- 	end
--- end
---
--- function mod:OnCombatEnd()
--- 	if self.Options.RangeFrame then
--- 		DBM.RangeCheck:Hide()
--- 	end
--- end
---
--- function mod:SPELL_AURA_APPLIED(args)
--- 	if args:IsSpellID(30403) then
--- 		warnArcaneInfusion:Show()
--- 	end
--- end
---
--- function mod:CHAT_MSG_MONSTER_YELL(msg)
--- 	if msg == L.DBM_CURA_YELL_OOM then
--- 		warnEvoSoon:Cancel()
--- 		warnEvo:Show()
--- 		timerNextEvo:Start()
--- 		timerEvo:Start()
--- 		warnEvoSoon:Schedule(95)
--- 	end
--- end
-
 --обычка--
-local timerEvo = mod:NewNextTimer(101, 30254) -- Прилив сил
+local timerEvo = mod:NewNextTimer(112, 30254) -- Прилив сил
 
 -- героик --
 local warnUnstableTar = mod:NewAnnounce("WarnUnstableTar", 3, 305309)
@@ -83,7 +39,7 @@ mod:AddNamePlateOption("Nameplate1", 305305, true)
 function mod:OnCombatStart()
 	if self:IsDifficulty("normal10") then
 		DBM:FireCustomEvent("DBM_EncounterStart", 15691, "The Curator")
-		timerEvo:Start()
+		timerEvo:Start(101)
 	elseif self:IsDifficulty("heroic10") then
 		DBM:FireCustomEvent("DBM_EncounterStart", 99974, "The Curator")
 		for i = 1, 3 do
@@ -99,12 +55,6 @@ function mod:OnCombatStart()
 		self.vb.isinCombat = true
 		self.vb.ter = true
 		table.wipe(unstableTargets)
-	end
-end
-
-function mod:CHAT_MSG_MONSTER_YELL(msg)
-	if msg == L.DBM_CURA_YELL_OOM then
-		timerEvo:Start(101)
 	end
 end
 
@@ -185,5 +135,15 @@ function mod:SPELL_CAST_SUCCESS(args)
 		if self.vb.ter then
 			self.vb.ter = false
 		end
+	elseif args:IsSpellID(30254) then
+		timerEvo:Start()
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if L.DBM_CURA_YELL_OOM and (msg == L.DBM_CURA_YELL_OOM or msg:find(L.DBM_CURA_YELL_OOM, 1, true)) then
+		timerEvo:Start()
+	elseif L.DBM_CURA_YELL_P2 and (msg == L.DBM_CURA_YELL_P2 or msg:find(L.DBM_CURA_YELL_P2, 1, true)) then
+		timerEvo:Stop()
 	end
 end
