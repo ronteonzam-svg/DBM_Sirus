8a93ee94bb06fd313d1fc909df71d85dd0972a43
DESKTOP-2E4LAF7\NeNikita <kolobikus@gmail.com>
2026-07-15 15:36:05 +0300

    [DBM-Karazhan] Zluker: Добавлено отслеживание цели Гравитационного отклонения и специальные предупреждения
    - Добавлена логика отслеживания цели заклинания 'Гравитационное отклонение' (305537) с помощью сканирования цели босса (BossTargetScanner).
    - Реализовано персональное предупреждение бежать (Run) для мили-игроков, если они являются целью заклинания, и стандартное объявление цели для остальных.
    - Добавлена опция автоматической установки метки (череп/Skull) на цель заклинания и её снятие при завершении или прерывании каста.
    - Скорректировано время перезарядки timerMagicCD (изменено с 79 до 46 сек).
    - Закомментирован неточный таймер начала боя по крику (timerCombatStart).

--- DBM-Karazhan/Zluker.lua
+++ DBM-Karazhan/Zluker.lua
@@ -1,31 +1,68 @@
 local mod = DBM:NewMod("Zluker", "DBM-Karazhan")
 local L   = mod:GetLocalizedStrings()
 
-mod:SetRevision("20210502220000")
+mod:SetRevision("20260713234500")
 mod:SetCreatureID(10055, 100552, 100555)
 mod:RegisterCombat("yell", L.YellZluker)
+mod:SetUsedIcons(8)
 
 mod:RegisterEvents(
-	"SPELL_CAST_START 305535",
+	"SPELL_CAST_START 305535 305537",
+	"SPELL_CAST_SUCCESS 305537",
+	"SPELL_CAST_FAILED 305537",
 	"CHAT_MSG_MONSTER_YELL"
 )
 
-local timerMagicCD     = mod:NewCDTimer(79, 305535)
-local timerCombatStart = mod:NewCombatTimer(42)
--- local warnSound						= mod:NewSoundAnnounce()
+local timerMagicCD     = mod:NewCDTimer(46, 305535)
+--local timerCombatStart = mod:NewCombatTimer(42)
 
+-- Prominent special warnings in the center of the screen
+local specWarnGravityDefianceTarget = mod:NewSpecialWarningTarget(305537, nil, nil, nil, 1, 2)
+local specWarnGravityDefianceYou    = mod:NewSpecialWarningRun(305537, nil, nil, nil, 4, 2)
+
+mod:AddSetIconOption("SetIconOnGravityTarget", 305537, true, 0, { 8 })
+
+--Таймер до запуска. Непонятно иногда 79 секунд, иногда около 30-40
+--[[
 function mod:CHAT_MSG_MONSTER_YELL(msg)
 	if L.YellPull and (msg == L.YellPull or msg:find(L.YellPull)) then
 		timerCombatStart:Start()
 	end
 end
+--]]
 
 function mod:OnCombatStart()
 	timerMagicCD:Start(66)
+	self.gravityTargetName = nil
 end
 
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(305535) then
 		timerMagicCD:Start()
+	elseif args:IsSpellID(305537) then
+		print("DBM Debug: SPELL_CAST_START 305537, sourceGUID: " .. tostring(args.sourceGUID))
+		self:BossTargetScanner(args.sourceGUID, "GravityTarget", 0.01, 10)
 	end
 end
+
+function mod:GravityTarget(targetname)
+	print("DBM Debug: GravityTarget called with: " .. tostring(targetname))
+	if not targetname then return end
+	if targetname == UnitName("player") and self:IsMelee() then
+		specWarnGravityDefianceYou:Show()
+	else
+		specWarnGravityDefianceTarget:Show(targetname)
+	end
+	if self.Options.SetIconOnGravityTarget then
+		self.gravityTargetName = targetname
+		self:SetIcon(targetname, 8, 3)
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(305537) and self.gravityTargetName then
+		self:RemoveIcon(self.gravityTargetName)
+		self.gravityTargetName = nil
+	end
+end
+mod.SPELL_CAST_FAILED = mod.SPELL_CAST_SUCCESS
